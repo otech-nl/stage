a2c4b03eb490fca8df52b3932891d43e80b9f06e
Wilko <wilkovoogd@gmail.com>
2018-11-01 10:12:27 +0100

    verkeerde inloggegevenstest toegevoegd

--- inlog.feature
+++ inlog.feature
@@ -5,10 +5,26 @@ Functionaliteit: Inlog
 	Wil ik inloggen
 
 	Achtergrond:
-		Stel ik ben niet ingelogd
+	Stel ik ben niet ingelogd
+
+Scenario: verkeerd wachtwoord
+Als ik een geldige gebruikersnaam invoer
+En ik een verkeerd wachtwoord invoer
+En ik op de knop inloggen druk
+Dan krijg ik een verkeerd-wachtwoord-melding
+En word ik niet ingelogd
+
+Scenario: verkeerde gebruikersnaam
+Als ik een verkeerde gebruikersnaam invoer
+En ik een verkeerd wachtwoord invoer
+En ik op de knop inloggen druk
+Dan krijg ik een verkeerde-gebruikersnaam-melding
+En word ik niet ingelogd
 
 Scenario: inloggen
 Als ik een geldige gebruikersnaam invoer
 En ik het goede wachtwoord invoer
 En ik op de knop inloggen druk
 Dan kom ik op de pagina Tochten
+
+
--- steps/steps.py
+++ steps/steps.py
@@ -3,21 +3,32 @@
 
 @given('ik ben niet ingelogd')
 def step_logged_off(context):
-    
     loggedoff_url = '%s/login?next=%%2F' % context.base_url
-    print(loggedoff_url)
     context.browser.visit(loggedoff_url)
-    
 @when('ik een geldige gebruikersnaam invoer')
-def func(context):
+def vul_gebruikersnaam_in(context):
     context.browser.find_by_id('email').first.fill('admin')
-
+@when('ik een verkeerde gebruikersnaam invoer')
+def vul_verkeerde_gebruikersnaam_in(context):
+    context.browser.find_by_id('email').first.fill('verkeerd')
 @when('ik het goede wachtwoord invoer')
-def func1(context):
+def vul_wachtwoord_in(context):
     context.browser.find_by_id('password').first.fill('nimda')
+@when('ik een verkeerd wachtwoord invoer')
+def vul_verkeerd_wachtwoord_in(context):
+    context.browser.find_by_id('password').first.fill('verkeerd')
 @when('ik op de knop inloggen druk')
-def func2(context):
-    context.browser.find_by_id('submit').click
+def klik_inloggen(context):
+    context.browser.find_by_id('submit').first.click()
 @then('kom ik op de pagina Tochten')
-def func3(context):
-    pass
+def inlog_tochten_pagina(context):
+    assert context.browser.url == '%s/tocht/' % context.base_url
+@then('krijg ik een verkeerd-wachtwoord-melding')
+def verkeerd_wachtwoord_melding(context):
+    assert context.browser.is_text_present('Invalid password')
+@then('word ik niet ingelogd')
+def inlog_mislukt(context):
+    assert context.browser.url != '%s/tocht/' % context.base_url
+@then('krijg ik een verkeerde-gebruikersnaam-melding')
+def verkeerde_gebruikersnaam_melding(context):
+    assert context.browser.is_text_present('Specified user does not exist')
