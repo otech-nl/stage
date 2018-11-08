ba584cab4952dba110ba7be2a3e8d2c6cb169c30
Wilko <wilkovoogd@gmail.com>
2018-11-08 13:02:47 +0100

    import voor rapporten steps toegevoegd

--- steps/steps.py
+++ steps/steps.py
@@ -5,5 +5,6 @@ from ledenbeheer import steps
 from tochtenbeheer import steps
 from gebruikersbeheer import steps
 from rollenbeheer import steps
+from rapporten import steps
 #from behave import given, when, then
 
