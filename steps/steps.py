555d0ab58bcbf313048c5ccaafa7316a36e7c0e0
Wilko <wilkovoogd@gmail.com>
2018-11-08 11:50:19 +0100

    import voor rollenbeheer toegevoegd

--- steps/steps.py
+++ steps/steps.py
@@ -4,5 +4,6 @@ from uitloggen import steps
 from ledenbeheer import steps
 from tochtenbeheer import steps
 from gebruikersbeheer import steps
+from rollenbeheer import steps
 #from behave import given, when, then
 
