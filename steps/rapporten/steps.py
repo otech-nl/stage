6969048299356957eecedec3e0dc8d77ccd22e86
Vitz1988 <44800036+Vitz1988@users.noreply.github.com>
2018-11-13 13:04:48 +0100

    updated rapporten steps

--- steps/rapporten/steps.py
+++ steps/rapporten/steps.py
@@ -1,8 +1,9 @@
 from behave import given, when, then
-from pathlib import Path
+#from selenium import webdriver
+#driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+#from selenium.webdriver.common.keys import Keys
 import os.path
 import time
-import os
 
 @when('ik op de knop Rapporten druk')
 def klik_rapporten(context):
@@ -45,14 +46,9 @@ def check_adresgegevens_naw(context):
     assert context.browser.find_by_xpath('/html/body/div/table')
     
 @when('ik op de CSV link klik')
-def click_csv(context):
+def click_csvhb(context):
     context.browser.find_link_by_partial_href('rapport/hoofdbewoners/csv').first.click()
     time.sleep(1)
-    
-@then('ik navigeer naar Chrome downloads')
-def navigate_chromedownloads(context):
-    chromedownloads_url = 'chrome://downloads/'
-    context.browser.visit(chromedownloads_url) 
     
 @then('zie ik de voltooide download in de Downloads map')
 def check_download_complete(context):
@@ -62,3 +58,71 @@ def check_download_complete(context):
 def delete_download(context):
     os.remove(os.path.expanduser('~/Downloads/leden.csv'))
     
+@when("ik op de 'etiketten op naam'-link klik")
+def click_etiketten(context):
+    context.browser.find_link_by_partial_href('rapport/hoofdbewoners/etiketten').first.click()
+    
+@then('kom ik op een pagina met adresgegevens in makkelijk printbaar formaat')
+def check_page_etiketten(context):
+    assert context.browser.url == '%s/rapport/hoofdbewoners/etiketten' % context.base_url
+    assert context.browser.find_by_css('.etiketten')
+    
+@when("ik op de 'etiketten op postcodes'-link klik")
+def click_postcodes(context):
+    context.browser.find_link_by_partial_href('rapport/hoofdbewoners/postcode').first.click()
+    
+@then('kom ik op een pagina met postcodes in makkelijk printbaar formaat')
+def check_page_postcodes(context):
+    assert context.browser.url == '%s/rapport/hoofdbewoners/postcode' % context.base_url
+    assert context.browser.find_by_css('.etiketten')
+    
+@when('ik op de link klik naar Opzeggingen')
+def click_opzeggingen(context):
+    context.browser.find_link_by_partial_href('rapport/opgezegd').first.click()
+    
+@then('kom ik op de pagina met Opzeggingen')
+def check_page_opzeggingen(context):
+    assert context.browser.url == '%s/rapport/opgezegd' % context.base_url
+    
+@then('krijg ik een lijst met Opzeggingen te zien')
+def check_list_opzeggingen(context):
+    assert context.browser.find_by_css('.display')
+
+@when('ik op de link klik naar Ex-leden')
+def click_exleden(context):
+    context.browser.find_link_by_partial_href('rapport/ex').first.click()
+
+@then('kom ik op de pagina met Ex-leden')
+def check_page_exleden(context):
+    assert context.browser.url == '%s/rapport/ex' % context.base_url
+    
+@then('krijg ik een lijst met Ex-leden te zien')
+def check_list_exleden(context):
+    assert context.browser.find_by_css('.display')
+
+@when('ik op de link klik naar Categorie-overzicht')
+def click_categorie(context):
+    context.browser.find_link_by_partial_href('rapport/categorie').first.click()
+
+@then('kom ik op de pagina Categorie-overzicht')
+def check_page_categorie(context):
+    assert context.browser.url == '%s/rapport/categorie' % context.base_url
+
+@then('kan ik alle categorieen bekijken door op de links te klikken')
+def check_collapse_categorie(context):
+    # context.browser.find_by_css('div.panel:nth-child(1) > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
+    # time.sleep(3)
+    # context.browser.find_by_css('div.panel:nth-child(2) > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
+    # time.sleep(3)
+    # context.browser.find_by_css('div.panel:nth-child(3) > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
+    # time.sleep(3)
+    # context.browser.find_by_css('div.panel:nth-child(4) > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
+    # time.sleep(3)
+    # context.browser.find_by_css('div.panel:nth-child(5) > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
+    # time.sleep(5)
+    # context.browser.find_by_css('div.panel:nth-child(6) > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
+    # time.sleep(3)
+    # context.browser.find_by_css('div.panel:nth-child(7) > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
+
+    assert context.browser.find_by_id('categorie_1')
+    time.sleep(30)
