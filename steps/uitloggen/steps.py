7c8e39debab78adf6b9c4603828d36a8717fec00
Wilko <wilkovoogd@gmail.com>
2018-11-21 13:53:41 +0100

    inlogfunctie toegevoegd

--- steps/uitloggen/steps.py
+++ steps/uitloggen/steps.py
@@ -3,15 +3,18 @@ from behave import given, when, then
 def split_and_strip(src, sep=None):
     return [token.strip() for token in src.split(sep)]
 
+def log_in(context, email, password):
+    context.browser.find_by_id('email').first.fill(email)
+    context.browser.find_by_id('password').first.fill(password)
+    context.browser.find_by_id('submit').first.click()
+
 @given('ik ben ingelogd')
 def ingelogd_check(context):
     loggedoff_url = '%s/login?next=%%2F' % context.base_url
     base_url = context.base_url
     context.browser.visit(base_url)
     if context.browser.url == loggedoff_url:                                
-        context.browser.find_by_id('email').first.fill('admin')             #moet makkelijker kunnen
-        context.browser.find_by_id('password').first.fill('nimda')          #
-        context.browser.find_by_id('submit').first.click()                  #
+        log_in(context, 'admin', 'nimda')
     assert context.browser.url != loggedoff_url 
     
 @when('ik op de knop uitloggen druk')
