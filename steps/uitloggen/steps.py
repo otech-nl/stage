8ddf992d3d0ce5642b7d86db95620e1a0c944a5a
Wilko <wilkovoogd@gmail.com>
2018-11-22 10:29:49 +0100

    steps: 'stel ik ben ingelogd' verzekerd nu dat de het admin account bij de FLAL vereniging hoort omdat de meeste steps geschreven zijn ervan uitgaande dat dit het geval is.

--- steps/uitloggen/steps.py
+++ steps/uitloggen/steps.py
@@ -15,7 +15,12 @@ def ingelogd_check(context):
     context.browser.visit(base_url)
     if context.browser.url == loggedoff_url:                                
         log_in(context, 'admin', 'nimda')
-    assert context.browser.url != loggedoff_url 
+    assert context.browser.url != loggedoff_url
+    context.browser.visit('%s/user/1' % base_url)
+    if context.browser.find_by_id('vereniging_id').value != '2':                    #zodat de steps, omdat ze geschreven zijn
+        context.browser.find_option_by_text('FLAL').first.click()                   #voor een FLAL account, goed verlopen.
+        context.browser.find_by_xpath('//button[@type="submit"]').first.click()     #.
+    context.browser.visit(base_url)                                                 #.
     
 @when('ik op de knop uitloggen druk')
 def klik_uitloggen(context):
